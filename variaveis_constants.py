07baac3782c041a51726328e87ad8783003d8dcd
Jeandra Cristina <jeandracristinasr@gmail.com>
2025-04-02 16:51:26 -0300

    Update variaveis_constants.py

--- variaveis_constants.py
+++ variaveis_constants.py
@@ -9,3 +9,15 @@ print(nome, idade)
 dados_pessoais = f'meu nome e {nome.upper()} e tenho {idade} anos.'
 
 print(dados_pessoais)
+
+for i in [1, 2, 3, 4, 5]:
+    print(i)
+    for j in [1, 2, 3, 4, 5]:
+        print(j)
+        print(i + j)
+    print(i)
+print('done looping')
+
+x = 4
+x *= 2
+x -= 3
