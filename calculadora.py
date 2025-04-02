0f06f6a7f8578a8a82caa0ee840aa138774a2cfd
Jeandra Cristina <jeandracristinasr@gmail.com>
2025-04-02 17:03:52 -0300

    Update calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -20,8 +20,24 @@ def potencia_n (a,b):
 def raiz_quadrada_n (a):
     return print(a**(1/2)) 
 
-soma_n(3,2) 
+soma_n(3,9) 
 subtracao_n(28,5) 
 multiplicacao_n(6,2)
 divisao_n(8,2)
-potencia_n(2,5)    
+potencia_n(2,5)   
+
+"""
+#Programa de Potencia
+print("\nInício do programa de potência")
+base = int(input("Digite a base: "))
+expoente = int(input("Digite o expoente: "))
+resultado = 1
+contador = 1    #2^3 = 2*2*2
+while contador <= expoente:
+    resultado = base * resultado
+    contador = contador + 1
+print(f"{base} elevado a {expoente} é igual a {resultado:.2f}")  
+print("Fim")      
+    
+    """
+ 
