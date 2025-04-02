9c4b4aae8f0a44763f02bd12dda72f618ddf01f3
Jeandra Cristina <jeandracristinasr@gmail.com>
2025-04-02 17:00:48 -0300

    Update calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -7,3 +7,9 @@ def subtracao_n (a,b):
 
 def multiplicacao_n (a,b):
     return print(a * b)
+
+def divisao_n (a,b):
+    if (b <= 0):
+        return print("Não é possível dividir por zero")
+    else:
+        return print(a / b)
