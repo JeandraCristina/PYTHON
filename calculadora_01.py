9d51ce496c23568bd5978c3bf14a8fa0107e5f4c
Jeandra Cristina <jeandracristinasr@gmail.com>
2025-04-07 20:18:46 -0300

    Update calculadora_01.py

--- calculadora_01.py
+++ calculadora_01.py
@@ -5,3 +5,6 @@ c = 's'
 while c == "s":
     a = float(input("Digite o primeiro numero: "))
     b = float(input("Digite o segundo numero: "))
+    print("Escolha uma função matematica.")
+    e = int(input("1 - Soma\n2 - Subtração\n3 - Multiplicação\n4 - Divisão\n5 - Potência\n: "))
+    
