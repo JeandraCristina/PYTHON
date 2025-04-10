af776306189bc0bd98e3e1ed4a4c310d97946224
Jeandra Cristina <jeandracristinasr@gmail.com>
2025-04-10 18:37:39 -0300

    Update Funcoes_Python.py

--- Funcoes_Python.py
+++ Funcoes_Python.py
@@ -7,3 +7,11 @@ A sintaxe básica para criar uma função em Python é a seguinte:
 def nome_da_funcao(argumentos):
     # corpo da função
     return resultado
+    
+A palavra-chave "def" indica o início da definição da função.
+"nome_da_funcao" é o nome que você escolhe para identificar a função. 
+É uma boa prática usar nomes descritivos que indiquem o que a função faz.
+"argumentos" são valores que você pode passar para a função para que ela possa trabalhar com eles.
+Os argumentos são opcionais e você pode ter quantos quiser (ou nenhum). 
+Eles são separados por vírgulas.
+    
