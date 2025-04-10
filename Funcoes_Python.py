b0a5ba53c5a423a90886b4665a81871f4abfc35d
Jeandra Cristina <jeandracristinasr@gmail.com>
2025-04-10 18:37:15 -0300

    Update Funcoes_Python.py

--- Funcoes_Python.py
+++ Funcoes_Python.py
@@ -3,3 +3,7 @@ Uma função em Python é uma estrutura que permite agrupar um
 conjunto de instruções para que possam ser executadas várias vezes,
 sem que seja necessário repetir todo o código. 
 A sintaxe básica para criar uma função em Python é a seguinte:
+
+def nome_da_funcao(argumentos):
+    # corpo da função
+    return resultado
