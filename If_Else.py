5c73294f5b59025da4983a123e2c08908cccd278
Jeandra Cristina <jeandracristinasr@gmail.com>
2025-04-16 20:44:04 -0300

    Update If_Else.py

--- If_Else.py
+++ If_Else.py
@@ -13,4 +13,10 @@ numero = float(input("Digite um número: "))
 if numero >= 0:
     print(f'O numero {numero} é positivo')
 else:
-    print(f'O numero {numero} é negativo')            
+    print(f'O numero {numero} é negativo')    
+    
+#Usando a condicioal If, faça um programa que verifique a letra digita pelo usuário:
+
+#Se a letra for igual a 'F' retorne 'Feminino'
+#Se a letra for igual a 'M' retorne 'Masculino'
+#Qualquer outra letra retorne 'Sexo inválido'            
