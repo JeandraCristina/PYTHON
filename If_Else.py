fa61d46632ee9e06b26e2ad581de375b4f62f40a
Jeandra Cristina <jeandracristinasr@gmail.com>
2025-04-16 20:41:44 -0300

    Update If_Else.py

--- If_Else.py
+++ If_Else.py
@@ -1,3 +1,6 @@
 #01 - Usando a conficional If, faça um programa que peça dois números diferentes ao usuário e retorne qual deles é o maior número.
 num1 = int(input('DIgite o 1° numero: '))
 num2 = int(input('DIgite o 2° numero: '))
+
+if num1 > num2:
+    print(f'O numero {num1} é maior') 
