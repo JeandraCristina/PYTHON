a7dcf21b18c1efda438a35c8640737ac9092aac2
Jeandra Cristina <jeandracristinasr@gmail.com>
2025-04-22 14:59:04 -0300

    Update If_Else.py

--- If_Else.py
+++ If_Else.py
@@ -45,4 +45,6 @@ print('--'*15)
 #Senão retorne 'Você é menor de idade.'
 idade = int(input('Digite sua idade'))
 if idade > 18:
-                                
+    print(f'Você é maior de idade') 
+else:
+    print(f'Você é menor de idade')                                
