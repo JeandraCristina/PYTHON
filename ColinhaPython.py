138dc149a4e2463aa434b67fa02f837cf1ca6233
Jeandra Cristina <jeandracristinasr@gmail.com>
2025-06-06 18:25:18 -0300

    Update ColinhaPython.py

--- ColinhaPython.py
+++ ColinhaPython.py
@@ -1,11 +1,12 @@
 # Colinha para Python
 
 # TIPOS DE DADOS 
-    """
+   """
     Booleano = True/False
     Inteiro = 10
     Flutuante = 10.5
     Palavra = 'Python'
     Lista = ['Valor1', 'Valor2']
+    Dicionário = [Chave1:Valor1, Chave2:Valor2]
     
     """
